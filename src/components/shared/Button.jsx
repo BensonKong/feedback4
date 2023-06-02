61806bcb04bcb277d6aecd335053ac62b6c00260
Benson Kong <binbing876@gmail.com>
2023-06-02 12:41:08 +0800

    button add comment

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-//this is button
+//this is buttondwdwed
 const Button = ({ children, version, type, isDisabled }) => {
   return (
     <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
